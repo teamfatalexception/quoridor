73ee51bbbaee3e465be8ed27b4e99c5eb3fe623f
Andrew Valancius <valancal199@potsdam.edu>
2016-04-03 17:47:22 -0400

    Update Player object fields and constuctors

--- src/main/java/quoridorFE/Player.java
+++ src/main/java/quoridorFE/Player.java
@@ -5,12 +5,24 @@ public class Player {
 
 	// Private fields
 	private int ID;  // Which number player he is.
-	private String name; // What team made him.
+	private String prefix; // team prefix
+	private String name; // Player name
 	private int port;
 	private int wallsLeft; // How many walls he can still place.
 	private int X, Y;
 
 	// Constructor
+	public Player(int ID, String prefix, String name, int wallsLeft) {
+		this.ID = ID;
+		this.prefix = prefix;
+        this.name = name;
+        this.port = -1;
+        this.wallsLeft = wallsLeft;
+       	this.X = -1;
+	 	this.Y = -1;
+	}
+	
+	// FIXME can we please depreciate this constructor?     AV - 4/3/2016
 	public Player(int ID, String name, int port, int wallsLeft, int startingX, int startingY){
 
 	     	this.ID = ID;
