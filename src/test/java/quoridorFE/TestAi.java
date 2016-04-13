54ee60e75cab1ec6cdf847f91e05767403a67183
Andrew Valancius <valancal199@potsdam.edu>
2016-04-13 15:53:46 -0400

    Add test to highlight the weakness of weak AI

--- src/test/java/quoridorFE/TestAi.java
+++ src/test/java/quoridorFE/TestAi.java
@@ -56,5 +56,50 @@ public class TestAi {
 		// tested triple jump
 		
 	}
+	
+	@Ignore
+	@Test
+	public void testGetShitMoveWithBlockingEnemy() {
+		QuoridorBoard testBoard = new QuoridorBoard(new Player(1, "TST", "test1", 5), 
+													new Player(2, "TST", "test2", 5),
+													new Player(3, "TST", "test3", 5),
+													new Player(4, "TST", "test4", 5));
+
+			String str = "(4, 1)";
+			String cmpstr = FEai.getShitMove(1, testBoard);
+			assertEquals("Expected " + str, str, cmpstr);
+			
+			testBoard.movePawn(1, 4, 1);
+			testBoard.movePawn(1, 4, 2);
+			testBoard.movePawn(1, 4, 3);
+			testBoard.movePawn(1, 4, 4);
+			testBoard.movePawn(1, 4, 5);
+			// pawn 1 is at (4, 5)
+			testBoard.movePawn(2, 4, 7);
+			testBoard.movePawn(2, 4, 6);
+			// pawn 2 is at (4, 6)
+			testBoard.movePawn(3, 0, 5);
+			testBoard.movePawn(3, 0, 6);
+			testBoard.movePawn(3, 0, 7);
+			testBoard.movePawn(3, 1, 7);
+			testBoard.movePawn(3, 2, 7);
+			testBoard.movePawn(3, 3, 7);
+			testBoard.movePawn(3, 4, 7);
+			// pawn 3 is at (4, 7)
+			testBoard.movePawn(4, 8, 5);
+			testBoard.movePawn(4, 8, 6);
+			testBoard.movePawn(4, 8, 7);
+			testBoard.movePawn(4, 8, 8);
+			testBoard.movePawn(4, 7, 8);
+			testBoard.movePawn(4, 6, 8);
+			testBoard.movePawn(4, 5, 8);
+			testBoard.movePawn(4, 4, 8);
+			// pawn 4 is at (4, 8)
+			
+			// 4/13/2014 AV - This test is here to highlight how the shit AI might return a bad move if it's shortest path to victory ends on an occupied space. 
+			
+			cmpstr = FEai.getShitMove(1, testBoard);
+			assertTrue("Expected a valid move to be returned by FEai.getShitMove()", testBoard.isValidMove(1, Integer.parseInt(Character.toString(cmpstr.charAt(1))), Integer.parseInt(Character.toString(cmpstr.charAt(4)))));
+	}
 
 }
