4ac1c27d1fc5d5bdd7b464e8e48d4f1078ad3a1c
Andrew Valancius <valancal199@potsdam.edu>
2016-04-13 13:45:02 -0400

    Make the weakest FEai's tests more robust.
    Added double jump and triple jump test cases.

--- src/test/java/quoridorFE/TestAi.java
+++ src/test/java/quoridorFE/TestAi.java
@@ -14,7 +14,8 @@ public class TestAi {
 													new Player(4, "TST", "test4", 5));
 		
 		String str = "(4, 1)";
-		assertEquals("Expected " + str, str, FEai.getShitMove(1, testBoard));
+		String cmpstr = FEai.getShitMove(1, testBoard);
+		assertEquals("Expected " + str, str, cmpstr);
 		
 		testBoard.movePawn(1, 4, 1);
 		testBoard.movePawn(1, 4, 2);
@@ -25,7 +26,34 @@ public class TestAi {
 		testBoard.movePawn(2, 4, 5);
 		// pawn 1 is at (4, 4) and 2 is at (4, 5)
 		str = "(4, 6)";
-		assertEquals("Expected " + str, str, FEai.getShitMove(1, testBoard));
+		cmpstr = FEai.getShitMove(1, testBoard);
+		assertEquals("Single jump, expected " + str + " Recieved: " + cmpstr, str, cmpstr);
+		// tested single jump
+		
+		testBoard.movePawn(3, 0, 5);
+		testBoard.movePawn(3, 0, 6);
+		testBoard.movePawn(3, 1, 6);
+		testBoard.movePawn(3, 2, 6);
+		testBoard.movePawn(3, 3, 6);
+		testBoard.movePawn(3, 4, 6);
+		// pawn 1 is at (4, 4), 2 is at (4, 5), and 3 is at (4, 6)
+		str = "(4, 7)";
+		cmpstr = FEai.getShitMove(1, testBoard);
+		assertEquals("Double jump, expected " + str + " Recieved: " + cmpstr, str, cmpstr);
+		// tested double jump
+		
+		testBoard.movePawn(4, 8, 5);
+		testBoard.movePawn(4, 8, 6);
+		testBoard.movePawn(4, 8, 7);
+		testBoard.movePawn(4, 7, 7);
+		testBoard.movePawn(4, 6, 7);
+		testBoard.movePawn(4, 5, 7);
+		testBoard.movePawn(4, 4, 7);
+		// pawn 1 is at (4, 4), 2 is at (4, 5), 3 is at (4, 6), and 4 is at (4, 7)
+		str = "(4, 8)";
+		cmpstr = FEai.getShitMove(1, testBoard);
+		assertEquals("Triple jump, expected " + str + " Recieved: " + cmpstr, str, cmpstr);
+		// tested triple jump
 		
 	}
 
